82cb274f59d2fe1c76b82cde8757a1abf0fa0aa6
abdalrzag eisa <bmjfdrh@gmail.com>
2022-11-06 01:21:51 +0300

    nerd_font_patch script

--- nerd_font_patch.sh
+++ nerd_font_patch.sh
@@ -1,3 +1,64 @@
 #!/bin/env bash
 
+set -eu
 
+if ! command -v fontforge &> /dev/null
+then
+    echo "Please install fontforge first!"
+    exit 1
+fi
+
+if [ ! -d ./dist/iosevka-storm/ ]
+then
+    echo "Please build font first!"
+    exit 1
+fi
+
+log() {
+    local message="$1"
+
+    echo "[INFO] $message"
+}
+
+MAIN_DIR="patch_with_nerd"
+FONTS_DIR="${MAIN_DIR}/fonts"
+DIST_DIR="${MAIN_DIR}/dist/iosevka-storm"
+
+if [ -d ${FONTS_DIR}/ ]
+then
+    rm -rf ${FONTS_DIR}
+fi
+
+if [ -d ${DIST_DIR}/ ]
+then
+    rm -rf ${DIST_DIR}
+fi
+
+mkdir -pv ${MAIN_DIR}/
+mkdir -pv ${FONTS_DIR}
+mkdir -pv ${DIST_DIR}
+
+log "Copying font files from dist/iosevka-storm/ttf into ${FONTS_DIR}/"
+cp -r ./dist/iosevka-storm/ttf/* ${FONTS_DIR}/
+
+cd ${MAIN_DIR} || :
+
+if [ -d ./nerd-fonts/ ]
+then
+    log "Nerd-font dir found!"
+    log "Pulling latest changes from 'https://github.com/ryanoasis/nerd-fonts'"
+    cd nerd-fonts/
+    git pull
+    cd ..
+else
+    log "Nerd-font dir not found!"
+    log "Git cloning from 'https://github.com/ryanoasis/nerd-fonts'"
+    git clone "https://github.com/ryanoasis/nerd-fonts.git"
+fi
+
+cp -r nerd-fonts/src .
+cp nerd-fonts/font-patcher .
+
+log "Patching all fonts inside of ${FONTS_DIR} dir and output to ${DIST_DIR}"
+find fonts -type f \
+    -exec fontforge -script font-patcher -c --use-single-width-glyphs --careful {} -out "dist/iosevka-storm/" \;
